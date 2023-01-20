1f06e16e493c30d1ea89fbcf49ab416426e84d37
v88-clifford-madrinan <johncliffordmadrinan838@gmail.com>
2023-01-20 09:25:18 +0800

    Register page is now functional

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const { render } = require("ejs");
 const express = require("express");
+const bcrypt = require("bcrypt");
 const app = express();
 
+/** USER DATA WOULD BE SAVED HERE INSTEAD OF DATABASE. THIS IS FOR DEMO PURPOSE ONLY. */
+const users = [];
+
 app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs", { name: "Clifford" });
@@ -20,8 +24,23 @@ app.get("/register", (req, res) => {
     res.render("register.ejs");
 });
 
-app.post("/register", (req, res) => {
-    // res.render("register.ejs");
+app.post("/register", async (req, res) => {
+    try{
+        const hashed_password = await bcrypt.hash(req.body.password, 10);
+
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashed_password
+        });
+
+        res.redirect("/login");
+    }
+    catch{
+        res.redirect("/register");
+    }
+    console.log(users);
 });
 
 app.listen(3000, () => console.log("Server running in port 3000..."));
