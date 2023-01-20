c56c22cc7cfeb0baae34d826a9009940742a4e3b
v88-clifford-madrinan <johncliffordmadrinan838@gmail.com>
2023-01-20 08:45:23 +0800

    Routes and views for home, login and register page was created

---
+++ server.js
@@ -0,0 +1,19 @@
+const { render } = require("ejs");
+const express = require("express");
+const app = express();
+
+app.set("view-engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { name: "Clifford" });
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+});
+
+app.listen(3000, () => console.log("Server running in port 3000..."));
